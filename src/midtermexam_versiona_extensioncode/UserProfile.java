3579de7fec5a82a851a47f9d84ecabdb111f2033
nancymadotra <157523056+nancymadotra@users.noreply.github.com>
2024-02-20 12:16:28 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -12,7 +12,10 @@ package midtermexam_versiona_extensioncode;
  * Students are reminded to add themselves as modifiers when editing the
  * code.
  * @author dancye, 2019
+ * user is Nancy
+ * Student Id 991722956
  */
+
 public class UserProfile 
 {
     
